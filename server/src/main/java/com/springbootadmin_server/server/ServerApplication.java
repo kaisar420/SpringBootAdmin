9693f9583d0bdb5e354dca8e444b084910b64d74
DESKTOP-HJTNC7V\iparhan <1486103249@qq.com>
2018-10-19 16:43:26 +0800

    kaisar

---
+++ server/src/main/java/com/springbootadmin_server/server/ServerApplication.java
@@ -0,0 +1,16 @@
+package com.springbootadmin_server.server;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
